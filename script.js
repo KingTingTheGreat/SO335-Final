7f7cc2e29342b1114341c693c0bae3b8217371b5
Jeffrey Ting <jting@bu.edu>
2024-05-06 14:37:44 -0400

    good enough white gradient

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // add circles to div id="white" with increasing size and decreasing opacity
 var white = document.getElementById("white");
 const numCircles = 50;
-for (i = 0; i < 10; i++) {
+for (i = 0; i < numCircles; i++) {
 	var circle = document.createElement("div");
-	circle.style.width = 1 + i * 20 + "px";
-	circle.style.height = 1 + i * 20 + "px";
-	circle.style.opacity = 1 - i * 6 * (1 / numCircles);
+	circle.style.width = 2 + i * 40 + "px";
+	circle.style.height = 2 + i * 40 + "px";
+	circle.style.opacity = 1 - i * 4 * (1 / numCircles);
 	circle.style.borderRadius = "50%";
 	circle.style.backgroundColor = "rgba(255, 255, 255)";
 	circle.style.position = "absolute";
